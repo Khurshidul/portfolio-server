daf2cdc27d779f3574b6822534f22feafa3d615a
Khurshidul <khurshidulalam5@gmail.com>
2022-12-27 00:53:03 +0600

    vercel config

--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ const app = express();
 const blogs = require("./routes/blog");
 
 mongoose
-  .connect("mongodb://localhost/portfolio")
+  .connect("mongodb+srv://portfolio:port1234@cluster0.pxqzsrt.mongodb.net/?retryWrites=true&w=majority")
   .then(() => console.log("server successfully connected"))
   .catch(err => console.error("something is wrong...", err.message));
 
 app.use(cors());
 app.use(express.json());
 app.use("/blogs", blogs);
+
+
+
+
 
 const port = process.env.PORT || 1000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- src/routes/blog.js
+++ src/routes/blog.js
@@ -11,12 +11,17 @@ router.get("/", async (req, res) => {
   }
 });
 router.get("/:id", async (req, res) => {
-  const id = req.params.id;
+  try{
+    const id = req.params.id;
   const query = { _id: ObjectId(id) };
   const blog = await Blog.findOne(query);
   res.send(blog);
+  }catch (error) {
+    console.error(error);
+  }
 });
 router.post("/", async (req, res) => {
+ try{
   const { error } = validateBlog(req.body);
   if (error) return res.status(400).send(error.details[0].message);
   let blog = await Blog.findOne({ title: req.body.title });
@@ -30,5 +35,8 @@ router.post("/", async (req, res) => {
   });
   await blog.save();
   res.send(blog);
+ }catch (error) {
+  console.error(error);
+}
 });
 module.exports = router;
